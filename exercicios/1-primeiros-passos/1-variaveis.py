027053b86c2fad99a75d1ecf0a05df73f7362edc
leoberberick <leoberberick@gmail.com>
2024-07-25 22:01:26 +0000

    Primeiros Passos String

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,11 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Manuela'
+apelido = 'Manu'
 # Declare variáveis de cada tipo numérico
-
-
+numero_inteiro = 6
+salario = 1515.00
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
